fbdf3ed64427b057d9e403c95907606e7b36a122
Romain <romainnith@hotmail.com>
2018-03-01 15:15:58 -0500

    servo testing arduino code

--- Arduino/Servo_Test/Servo_Test.ino
+++ Arduino/Servo_Test/Servo_Test.ino
@@ -1,11 +1,21 @@
 #include <Servo.h>
 
-void setup() {
-  // put your setup code here, to run once:
+// Variable declarations
+Servo myServo;
+int servoPin = 10; // Change according to board
+int servoAngle = 0;
 
+void setup() {
+  Serial.begin(9600);        // set up Serial library at 9600 bps
+
+  myServo.attach(servoPin);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  if (Serial.available() > 0){
+    servoAngle = Serial.read();
+    myServo.write(servoAngle);
+    Serial.println("Servo angle set at " + myServo.read());
+  }
+  
 }
