5d0d89381df7f9c73af92ee35e18bb16411280b0
Romain <romainnith@hotmail.com>
2018-03-23 21:54:31 -0400

    Unity Arduino integration start

--- Arduino/Servo_Test/Servo_Test.ino
+++ Arduino/Servo_Test/Servo_Test.ino
@@ -3,7 +3,7 @@
 // Variable declarations
 Servo myServo;
 int servoPin = 10; // Change according to board
-int servoAngle = 0;
+int servoAngle = 90;
 
 void setup() {
   Serial.begin(9600);        // set up Serial library at 9600 bps
@@ -13,9 +13,19 @@ void setup() {
 
 void loop() {
   if (Serial.available() > 0){
-    servoAngle = Serial.read();
-    myServo.write(servoAngle);
-    Serial.println("Servo angle set at " + myServo.read());
+    servoAngle = Serial.parseInt();
+
+    if (servoAngle == 300){
+      myServo.detach();
+    }
+    else{
+      myServo.attach(servoPin);
+      myServo.write(servoAngle);
+      Serial.print("Servo angle set at ");
+      Serial.print(myServo.read(), DEC);
+      Serial.print(" Read: ");
+      Serial.println(servoAngle);
+    }
+    
   }
-  
 }
